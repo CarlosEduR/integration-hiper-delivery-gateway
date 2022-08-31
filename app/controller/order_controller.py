75c0e55fd1e2b30ffb1f721afff28b45af33dab2
Carlos Eduardo <x07196846176@tcu.gov.br>
2022-08-31 02:42:30 -0300

    Update path of endpoints, to better divide location inside nginx

--- app/controller/order_controller.py
+++ app/controller/order_controller.py
@@ -2,13 +2,13 @@ from main import application as app
 from sanic.response import text
 from app.service.hiper.hiper_service import HiperService
 
-@app.get("/health-check")
+@app.get("hiper-delivery/health-check")
 async def home(request):
     print("Health checking...")
     return text("Hello, World!")
 
 
-@app.post("/api/webhook")
+@app.post("hiper-delivery/api/webhook")
 async def order_event(request):
     HiperService().register_order(request.json["order"])
     return text("Hello, Webhook")
